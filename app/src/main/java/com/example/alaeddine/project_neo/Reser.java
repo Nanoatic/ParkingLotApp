975b6efc1b973ea9410b6aa2a5ef678bf51d191d
FelYous <yousseffalleh@gmail.com>
2017-10-24 15:44:54 +0100

    Res avec java

---
+++ app/src/main/java/com/example/alaeddine/project_neo/Reser.java
@@ -0,0 +1,13 @@
+package com.example.alaeddine.project_neo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reser);
+    }
+}
